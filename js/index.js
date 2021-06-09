3526493ac50b147172b92574ae3b72dbee02ade7
Erik Sargsyan <erosargsyan7@gmail.com>
2021-06-09 13:29:11 +0400

    feat: add timer js

---
+++ js/index.js
@@ -0,0 +1,17 @@
+let x = setInterval(function () {
+  let now = new Date().getTime();
+  let distance = countDownDate - now;
+  let day = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hour = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let min = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let sec = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("day").innerHTML = day;
+  document.getElementById("hour").innerHTML = hour;
+  document.getElementById("min").innerHTML = min;
+  document.getElementById("sec").innerHTML = sec;
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+let countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
